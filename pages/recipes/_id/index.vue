92a9866d747ad864a31cd0a38e8e4b31bb5bdfdc
001895 <feryindo5@gmail.com>
2019-11-20 12:59:38 +0700

    commit 1

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- <p>{{$route.params.id}}</p> -->
+  <section class="single-recipe">
+    <h1>{{recipe.title}}</h1>
+    <div>
+      <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title" />
+    </div>
+    <p>{{recipe.descriptionText}}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipe: [
+            {
+              id: "1",
+              title: "God of War",
+              previewText: "Awesome PS4 Game",
+              descriptionText: "Only On PlayStation",
+              thumbnail:
+                "https://media.gamestop.com/i/gamestop/10131619/God-of-War"
+            },
+            {
+              id: "2",
+              title: "Fortnite",
+              previewText: "Awesome PS4 Game",
+              descriptionText: "Only On PlayStation",
+              thumbnail:
+                "https://i5.walmartimages.com/asr/a9409438-408f-4945-b576-3debe927bbbb_1.4b01cf95cfdcfff6d730483d926e9949.jpeg"
+            }
+          ].find(el => el.id === context.params.id)
+        });
+      }, 1500);
+    });
+  }
+};
+</script>
+
+<style scoped>
+.single-recipe {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 30px;
+}
+
+.recipe-image {
+  width: 100%;
+}
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="recipes">
+    <Recipe
+      v-for="recipe in recipes"
+      :key="recipe.id"
+      :thumbnail="recipe.thumbnail"
+      :title="recipe.title"
+      :id="recipe.id"
+      :previewText="recipe.previewText"
+    />
+
+    <!-- <article class="recipe">
+      <div
+        class="thumbnail"
+        style="background-image: url('https://i5.walmartimages.com/asr/a9409438-408f-4945-b576-3debe927bbbb_1.4b01cf95cfdcfff6d730483d926e9949.jpeg')"
+      ></div>
+      <h1>Title</h1>
+      <p>Happy Good-Day</p>
+    </article>-->
+  </section>
+</template>
+
+<script>
+import Recipe from "@/components/Recipe";
+import { resolve, reject } from "q";
+export default {
+  components: {
+    Recipe
+  },
+  asyncData() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipes: [
+            {
+              id: "1",
+              title: "God of War",
+              previewText: "Awesome PS4 Game",
+              thumbnail:
+                "https://media.gamestop.com/i/gamestop/10131619/God-of-War"
+            },
+            {
+              id: "2",
+              title: "Fortnite",
+              previewText: "Awesome PS4 Game",
+              thumbnail:
+                "https://i5.walmartimages.com/asr/a9409438-408f-4945-b576-3debe927bbbb_1.4b01cf95cfdcfff6d730483d926e9949.jpeg"
+            }
+          ]
+        });
+      }, 1500);
+    });
+  }
+};
+</script>
+
+<style scoped>
+.recipes {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+}
+</style>
